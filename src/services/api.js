cbf872c4c79f2c1084b0e2334cc270747ace1482
Jigar Jaware <jrjaware@gmail.com>
2021-03-23 17:19:04 +0530

    update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://git.heroku.com/fixydate-api.git'
+    baseURL:'https://fixydate-api.herokuapp.com/'
 })
 
 export default api;
